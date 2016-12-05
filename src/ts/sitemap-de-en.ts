659534b9138cba037e22386d6f92b892a9903e7a
gabriel-rausch <mail@gabriel-rausch.de>
2016-12-05 16:26:50 +0100

    update sitemap script and tiny style adjustments for formular module

--- src/ts/sitemap-de-en.ts
+++ src/ts/sitemap-de-en.ts
@@ -12,27 +12,21 @@ xmlSitemap {
     }
     wrap (
         <?xml version="1.0" encoding="UTF-8"?>
-    <urlset
-        xmlns="http://www.google.com/schemas/sitemap/0.84"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xsi:schemalocation="http://www.google.com/schemas/sitemap/0.84 www.google.com/schemas/sitemap/0.84/sitemap.xsd
-    ">|</urlset>
-)
+        <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">|</sitemapindex>
+    )
 
     10 = COA
     10 {
         10 = TEXT
         10.value (
-            <url>
+            <sitemap>
                 <loc>http://kadia.de/sitemap-de.xml</loc>
-                <lastmod>2016-09-30T09:53:46Z</lastmod>
-                <priority>1.0</priority>
-            </url>
-            <url>
+                <lastmod>2016-12-05T09:23:34Z</lastmod>
+            </sitemap>
+            <sitemap>
                 <loc>http://kadia.de/sitemap-en.xml</loc>
-                <lastmod>2016-09-30T09:53:46Z</lastmod>
-                <priority>1.0</priority>
-            </url>
+                <lastmod>2016-12-05T09:23:34Z</lastmod>
+            </sitemap>
         )
     }
 
