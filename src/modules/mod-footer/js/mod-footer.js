083b3c60bbf35b68d20868f46ff4a695131c0435
gabriel-rausch <gabriel.rausch@1und1.de>
2017-05-09 16:55:56 +0200

    add form for newletter in footer 2

--- src/modules/mod-footer/js/mod-footer.js
+++ src/modules/mod-footer/js/mod-footer.js
@@ -1,3 +1,4 @@
+/*jshint loopfunc:true */
 goog.provide('kstatic.modules.footer');
 
 goog.require('goog.dom');
@@ -39,12 +40,17 @@ kstatic.modules.footer.prototype.initFooterNavi = function() {
 
 kstatic.modules.footer.prototype.initNewsletterForm = function() {
   var self = this;
-  var openForm = self.node.querySelector('.newsletter-open-form');
-  var form = self.node.querySelector('.newsletter-form');
+  var openForm = self.node.querySelectorAll('.newsletter-open-form');
+  var form = self.node.querySelectorAll('.newsletter-form');
 
-  goog.events.listen(openForm, goog.events.EventType.CLICK, function(e) {
-    e.preventDefault();
-    form.classList.add('show');
-  });
+  for (var i = 0; i < openForm.length; i++) {
+    goog.events.listen(openForm[i], goog.events.EventType.CLICK, function(e) {
+      e.preventDefault();
+
+      for (var j = 0; j < openForm.length; j++) {
+        form[j].classList.add('show');
+      }
+    });
+  }
 };
 
