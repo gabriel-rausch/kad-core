a858ef1da6876ce82dd152b492dad4144b2ca24d
gabriel-rausch <gabriel.rausch@1und1.de>
2017-06-22 09:36:41 +0200

    news detail

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -5,6 +5,9 @@
     <INCLUDE_TYPOSCRIPT: source="FILE: typo3conf/ext/ktempl/ts/powermail.ts">
     <INCLUDE_TYPOSCRIPT: source="FILE: typo3conf/ext/ktempl/ts/footer.ts">
 
+lib.pageID = TEXT
+lib.pageID.data = TSFE:id
+
 lib.mainnav = HMENU
 lib.mainnav {
     wrap = |
@@ -125,10 +128,106 @@ page.10 {
 }
 
 
-#[globalVar = TSFE:page|uid=4]
-#
+[globalVar = TSFE:page|uid=4]
+
+temp.newsPUIs = HMENU
+temp.newsPUIs {
+    special = directory
+    special.value = 4
+    entryLevel = 0
+
+    1 = TMENU
+    1 {
+        expAll = 1
+        noBlur = 1
+        NO.allStdWrap.field = uid
+        # start list with this uid and add sub pages with comma
+            NO.allStdWrap.wrap = this,| |*|  ,|
+    }
+    2 < .1
+}
+
 #page = PAGE
-#page.10 = TEXT
-#page.10.value = -_-_!
-#
-#[global]
+#page.10 < temp.newsPUIs
+
+page = PAGE
+page.5 = LOAD_REGISTER
+page.5 {
+    currentList = TEXT
+    currentList.cObject < temp.newsPUIs
+}
+page.10 = FLUIDTEMPLATE
+page.10 {
+    format = html
+    languageField = sys_language_uid
+    file = typo3conf/ext/ktempl/main.html
+    partialRootPath = typo3conf/ext/ktempl/
+    layoutRootPath = typo3conf/ext/ktempl/
+
+
+        dataProcessing {
+        10 = TYPO3\CMS\Frontend\DataProcessing\DatabaseQueryProcessor
+        10 {
+            table = tt_content
+            pidInList.cObject < temp.newsPUIs
+            orderBy.dataWrap = FIND_IN_SET(pid,'{register:currentList}'),|
+            languageField = sys_language_uid
+            as = content
+            dataProcessing {
+                10 = TYPO3\CMS\Frontend\DataProcessing\FilesProcessor
+                10 {
+                    languageField = sys_language_uid
+                    references.fieldName = assets
+                }
+            }
+        }
+    }
+}
+
+[global]
+
+
+
+[globalVar = TSFE:page|pid=4]
+
+page = PAGE
+page.10 = FLUIDTEMPLATE
+page.10 {
+    format = html
+    languageField = sys_language_uid
+    file = typo3conf/ext/ktempl/news-detailpage.html
+    partialRootPath = typo3conf/ext/ktempl/
+    layoutRootPath = typo3conf/ext/ktempl/
+
+    dataProcessing {
+        10 = TYPO3\CMS\Frontend\DataProcessing\DatabaseQueryProcessor
+        10 {
+            table = tt_content
+            orderBy = sorting
+            languageField = sys_language_uid
+            as = content
+            dataProcessing {
+                10 = TYPO3\CMS\Frontend\DataProcessing\FilesProcessor
+                10 {
+                    languageField = sys_language_uid
+                    references.fieldName = assets
+                }
+            }
+        }
+        20 = TYPO3\CMS\Frontend\DataProcessing\DatabaseQueryProcessor
+        20 {
+            table = tt_content
+            pidInList = 33
+            languageField = sys_language_uid
+            as = contentnews
+            dataProcessing {
+                10 = TYPO3\CMS\Frontend\DataProcessing\FilesProcessor
+                10 {
+                    languageField = sys_language_uid
+                    references.fieldName = assets
+                }
+            }
+        }
+    }
+}
+[global]
